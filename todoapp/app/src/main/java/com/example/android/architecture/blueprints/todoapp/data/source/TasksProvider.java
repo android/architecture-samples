68e078b805339ebe444e0116b29abffedfe997d5
David Gonzalez <malmstein@gmail.com>
2016-03-23 21:07:35 +0000

    adding ContentProvider and updating DataContract

---
+++ todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/data/source/TasksProvider.java
@@ -0,0 +1,176 @@
+package com.example.android.architecture.blueprints.todoapp.data.source;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+import com.example.android.architecture.blueprints.todoapp.data.source.local.TasksDbHelper;
+import com.example.android.architecture.blueprints.todoapp.data.source.local.TasksPersistenceContract;
+
+public class TasksProvider extends ContentProvider {
+
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private TasksDbHelper mTasksDbHelper;
+
+    private static final int TASK = 100;
+    private static final int TASK_ITEM = 101;
+
+    @Override
+    public boolean onCreate() {
+        mTasksDbHelper = new TasksDbHelper(getContext());
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        Cursor retCursor;
+        switch (sUriMatcher.match(uri)) {
+            case TASK:
+                retCursor = mTasksDbHelper.getReadableDatabase().query(
+                    TasksPersistenceContract.TaskEntry.TABLE_NAME,
+                    projection,
+                    selection,
+                    selectionArgs,
+                    null,
+                    null,
+                    sortOrder
+                );
+                break;
+            case TASK_ITEM:
+                retCursor = mTasksDbHelper.getReadableDatabase().query(
+                    TasksPersistenceContract.TaskEntry.TABLE_NAME,
+                    projection,
+                    TasksPersistenceContract.TaskEntry._ID + " = '" + ContentUris.parseId(uri) + "'",
+                    null,
+                    null,
+                    null,
+                    sortOrder
+                );
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        retCursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return retCursor;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case TASK:
+                return TasksPersistenceContract.CONTENT_TASK_TYPE;
+            case TASK_ITEM:
+                return TasksPersistenceContract.CONTENT_TASK_ITEM_TYPE;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        final SQLiteDatabase db = mTasksDbHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        Uri returnUri;
+
+        switch (match) {
+            case TASK:
+                Cursor exists = db.query(
+                    TasksPersistenceContract.TaskEntry.TABLE_NAME,
+                    new String[]{TasksPersistenceContract.TaskEntry.COLUMN_NAME_ENTRY_ID},
+                    TasksPersistenceContract.TaskEntry.COLUMN_NAME_ENTRY_ID + " = ?",
+                    new String[]{values.getAsString(TasksPersistenceContract.TaskEntry.COLUMN_NAME_ENTRY_ID)},
+                    null,
+                    null,
+                    null
+                );
+                if (exists.moveToLast()) {
+                    long _id = db.update(
+                        TasksPersistenceContract.TaskEntry.TABLE_NAME, values,
+                        TasksPersistenceContract.TaskEntry.COLUMN_NAME_ENTRY_ID + " = ?",
+                                         new String[]{values.getAsString(TasksPersistenceContract.TaskEntry.COLUMN_NAME_ENTRY_ID)}
+                    );
+                    if (_id > 0) {
+                        returnUri = TasksPersistenceContract.TaskEntry.buildTasksUriWith(_id);
+                    } else {
+                        throw new android.database.SQLException("Failed to insert row into " + uri);
+                    }
+                } else {
+                    long _id = db.insert(TasksPersistenceContract.TaskEntry.TABLE_NAME, null, values);
+                    if (_id > 0) {
+                        returnUri = TasksPersistenceContract.TaskEntry.buildTasksUriWith(_id);
+                    } else {
+                        throw new android.database.SQLException("Failed to insert row into " + uri);
+                    }
+                }
+                exists.close();
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return returnUri;
+
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        final SQLiteDatabase db = mTasksDbHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        int rowsDeleted;
+
+        switch (match) {
+            case TASK:
+                rowsDeleted = db.delete(
+                    TasksPersistenceContract.TaskEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        if (selection == null || rowsDeleted != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return rowsDeleted;
+
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        final SQLiteDatabase db = mTasksDbHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        int rowsUpdated;
+
+        switch (match) {
+            case TASK:
+                rowsUpdated = db.update(TasksPersistenceContract.TaskEntry.TABLE_NAME, values, selection,
+                                        selectionArgs
+                );
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        if (rowsUpdated != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return rowsUpdated;
+    }
+
+    private static UriMatcher buildUriMatcher() {
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = TasksPersistenceContract.CONTENT_AUTHORITY;
+
+        matcher.addURI(authority, TasksPersistenceContract.TaskEntry.TABLE_NAME, TASK);
+        matcher.addURI(authority, TasksPersistenceContract.TaskEntry.TABLE_NAME + "/*", TASK_ITEM);
+
+        return matcher;
+    }
+
+}
--- todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/data/source/local/TasksPersistenceContract.java
+++ todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/data/source/local/TasksPersistenceContract.java
@@ -16,6 +16,8 @@
 
 package com.example.android.architecture.blueprints.todoapp.data.source.local;
 
+import android.content.ContentUris;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -23,16 +25,43 @@ import android.provider.BaseColumns;
  */
 public final class TasksPersistenceContract {
 
+    public static final String CONTENT_AUTHORITY = "com.example.android.architecture.blueprints.todoapp";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String CONTENT_TASK_TYPE = "vnd.android.cursor.dir/" + CONTENT_AUTHORITY + "/" + TaskEntry.TABLE_NAME;
+    public static final String CONTENT_TASK_ITEM_TYPE = "vnd.android.cursor.item/" + CONTENT_AUTHORITY + "/" + TaskEntry.TABLE_NAME;
+
     // To prevent someone from accidentally instantiating the contract class,
     // give it an empty constructor.
-    public TasksPersistenceContract() {}
+    public TasksPersistenceContract() {
+    }
 
     /* Inner class that defines the table contents */
     public static abstract class TaskEntry implements BaseColumns {
+
+        public static String[] TASKS_COLUMNS = new String[]{
+                TasksPersistenceContract.TaskEntry._ID,
+                TasksPersistenceContract.TaskEntry.COLUMN_NAME_ENTRY_ID,
+                TasksPersistenceContract.TaskEntry.COLUMN_NAME_TITLE,
+                TasksPersistenceContract.TaskEntry.COLUMN_NAME_DESCRIPTION,
+                TasksPersistenceContract.TaskEntry.COLUMN_NAME_COMPLETED};
+
         public static final String TABLE_NAME = "task";
+
         public static final String COLUMN_NAME_ENTRY_ID = "entryid";
         public static final String COLUMN_NAME_TITLE = "title";
         public static final String COLUMN_NAME_DESCRIPTION = "description";
         public static final String COLUMN_NAME_COMPLETED = "completed";
+
+        public static final Uri CONTENT_TASK_URI = BASE_CONTENT_URI.buildUpon().appendPath(TABLE_NAME).build();
+
+        public static Uri buildTasksUriWith(long id) {
+            return ContentUris.withAppendedId(CONTENT_TASK_URI, id);
+        }
+
+        public static Uri buildTasksUri() {
+            return CONTENT_TASK_URI.buildUpon().build();
+        }
+
     }
 }
