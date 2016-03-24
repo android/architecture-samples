cf5493b01c390831504968bea40eaff7e78b7f25
David Gonzalez <malmstein@gmail.com>
2016-03-24 14:28:58 +0000

    added TaskCursorLoader

---
+++ todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/data/source/TaskCursorLoader.java
@@ -0,0 +1,27 @@
+package com.example.android.architecture.blueprints.todoapp.data.source;
+
+import android.content.Context;
+import android.support.v4.content.CursorLoader;
+
+import com.example.android.architecture.blueprints.todoapp.data.source.local.TasksPersistenceContract;
+
+public class TaskCursorLoader extends CursorLoader {
+
+    //  HNewsContract.StoryEntry.FILTER + " = ?",
+    //new String[]{Story.FILTER.ask.name()},
+
+    public TaskCursorLoader(Context context, long id) {
+        super(context, TasksPersistenceContract.TaskEntry.buildTasksUriWith(id), TasksPersistenceContract.TaskEntry.TASKS_SQL_COLUMNS, null, null, null);
+    }
+
+    @Override
+    protected void onStopLoading() {
+        cancelLoad();
+    }
+
+    @Override
+    protected void onReset() {
+        onStopLoading();
+    }
+
+}
