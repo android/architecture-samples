c19730b9a9d86d22f086c16221ceae91d0b55d7d
Jose Alcerreca <pepevi@gmail.com>
2016-08-22 17:39:40 +0100

    Re-applies fixes that were removed by merge

--- todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragment.java
+++ todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragment.java
@@ -119,6 +119,7 @@ public class TaskDetailFragment extends Fragment implements TaskDetailContract.V
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.taskdetail_fragment_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
     }
 
     @Override
@@ -190,8 +191,10 @@ public class TaskDetailFragment extends Fragment implements TaskDetailContract.V
             // If the task was edited successfully, go back to the list.
             if (resultCode == Activity.RESULT_OK) {
                 getActivity().finish();
+                return;
             }
         }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
