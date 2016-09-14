26ea3790f4d3be77c30c4496b0bd918b13fb0e6b
Jose Alcerreca <pepevi@gmail.com>
2016-09-14 16:24:56 +0100

    Fixes imports, test names and makes test more readable

--- todoapp/app/src/androidTestMock/java/com/example/android/architecture/blueprints/todoapp/addedittask/AddEditTaskScreenTest.java
+++ todoapp/app/src/androidTestMock/java/com/example/android/architecture/blueprints/todoapp/addedittask/AddEditTaskScreenTest.java
@@ -16,31 +16,28 @@
 
 package com.example.android.architecture.blueprints.todoapp.addedittask;
 
-import android.support.test.espresso.Espresso;
-import android.support.test.espresso.intent.rule.IntentsTestRule;
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
-import android.test.suitebuilder.annotation.LargeTest;
-
-import com.example.android.architecture.blueprints.todoapp.Injection;
-import com.example.android.architecture.blueprints.todoapp.R;
-import com.example.android.architecture.blueprints.todoapp.data.FakeTasksRemoteDataSource;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static android.support.test.InstrumentationRegistry.getTargetContext;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import com.example.android.architecture.blueprints.todoapp.R;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * Tests for the add task screen.
@@ -74,11 +71,10 @@ public class AddEditTaskScreenTest {
     }
 
     @Test
-    public void errorShownOnEmptyTask() {
+    public void emptyTask_isNotSaved() {
         // Add invalid title and description combination
-        onView(withId(R.id.add_task_title)).perform(typeText(""));
-        onView(withId(R.id.add_task_description)).perform(typeText(""),
-                closeSoftKeyboard());
+        onView(withId(R.id.add_task_title)).perform(clearText());
+        onView(withId(R.id.add_task_description)).perform(clearText());
         // Try to save the task
         onView(withId(R.id.fab_edit_task_done)).perform(click());
 
