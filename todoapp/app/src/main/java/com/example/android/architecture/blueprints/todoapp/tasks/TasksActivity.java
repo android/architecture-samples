22b4d5a82780fa1ecb3e3332d956a41c61beb52b
Mohamed Habib <mohamed.mo.habib@gmail.com>
2016-11-17 15:33:14 +0200

    removed StatisticsActivity and replaced the TasksFragment with StatisticsFragment instead of creating new activity each time i switch between tasks and statistics, now we have TasksActivity and when statistics item is selected the StatisticsFragment is replaced with the TasksFragment and when tasks item is selected the TasksFragment is replaced with the StatisticsFragment.

--- todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksActivity.java
+++ todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksActivity.java
@@ -16,11 +16,12 @@
 
 package com.example.android.architecture.blueprints.todoapp.tasks;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.VisibleForTesting;
 import android.support.design.widget.NavigationView;
 import android.support.test.espresso.IdlingResource;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -30,7 +31,9 @@ import android.view.MenuItem;
 
 import com.example.android.architecture.blueprints.todoapp.Injection;
 import com.example.android.architecture.blueprints.todoapp.R;
-import com.example.android.architecture.blueprints.todoapp.statistics.StatisticsActivity;
+import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository;
+import com.example.android.architecture.blueprints.todoapp.statistics.StatisticsFragment;
+import com.example.android.architecture.blueprints.todoapp.statistics.StatisticsPresenter;
 import com.example.android.architecture.blueprints.todoapp.util.ActivityUtils;
 import com.example.android.architecture.blueprints.todoapp.util.EspressoIdlingResource;
 
@@ -42,45 +45,81 @@ public class TasksActivity extends AppCompatActivity {
 
     private TasksPresenter mTasksPresenter;
 
+    private Bundle savedInstanceState;
+
+    private ActionBar ab;
+
+    private NavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tasks_act);
+        this.savedInstanceState = savedInstanceState;
 
         // Set up the toolbar.
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        ActionBar ab = getSupportActionBar();
-        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
-        ab.setDisplayHomeAsUpEnabled(true);
+        ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
 
         // Set up the navigation drawer.
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerLayout.setStatusBarBackground(R.color.colorPrimaryDark);
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
         if (navigationView != null) {
             setupDrawerContent(navigationView);
         }
 
-        TasksFragment tasksFragment =
-                (TasksFragment) getSupportFragmentManager().findFragmentById(R.id.contentFrame);
-        if (tasksFragment == null) {
-            // Create the fragment
+        showTasksFragment(savedInstanceState);
+    }
+
+    private void showTasksFragment(Bundle savedInstanceState) {
+        Fragment tasksFragment = getSupportFragmentManager().findFragmentById(R.id.contentFrame);
+        if (tasksFragment == null || !(tasksFragment instanceof TasksFragment)) {
+            // Create the fragment(View)
             tasksFragment = TasksFragment.newInstance();
-            ActivityUtils.addFragmentToActivity(
-                    getSupportFragmentManager(), tasksFragment, R.id.contentFrame);
+            ActivityUtils.addFragmentToActivity(getSupportFragmentManager(), tasksFragment, R.id.contentFrame);
         }
 
+        //Create the Repository
+        TasksRepository tasksRepository = Injection.provideTasksRepository(getApplicationContext());
+
         // Create the presenter
-        mTasksPresenter = new TasksPresenter(
-                Injection.provideTasksRepository(getApplicationContext()), tasksFragment);
+        mTasksPresenter = new TasksPresenter(tasksRepository, (TasksFragment) tasksFragment);
 
         // Load previously saved state, if available.
         if (savedInstanceState != null) {
-            TasksFilterType currentFiltering =
-                    (TasksFilterType) savedInstanceState.getSerializable(CURRENT_FILTERING_KEY);
+            TasksFilterType currentFiltering = (TasksFilterType) savedInstanceState.getSerializable(CURRENT_FILTERING_KEY);
             mTasksPresenter.setFiltering(currentFiltering);
         }
+
+        MenuItem mi1 = navigationView.getMenu().getItem(0);
+        MenuItem mi2 = navigationView.getMenu().getItem(1);
+        mi1.setChecked(true);
+        mi2.setChecked(false);
+    }
+
+    private void showStatisticsFragment() {
+        Fragment statisticsFragment = getSupportFragmentManager().findFragmentById(R.id.contentFrame);
+
+        if (statisticsFragment == null || !(statisticsFragment instanceof StatisticsFragment)) {
+            statisticsFragment = StatisticsFragment.newInstance();
+            ActivityUtils.addFragmentToActivity(getSupportFragmentManager(), statisticsFragment, R.id.contentFrame);
+        }
+
+        TasksRepository tasksRepository = Injection.provideTasksRepository(getApplicationContext());
+
+        new StatisticsPresenter(tasksRepository, (StatisticsFragment) statisticsFragment);
+
+        MenuItem mi1 = navigationView.getMenu().getItem(0);
+        MenuItem mi2 = navigationView.getMenu().getItem(1);
+        mi1.setChecked(false);
+        mi2.setChecked(true);
+
     }
 
     @Override
@@ -101,27 +140,29 @@ public class TasksActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void setupDrawerContent(NavigationView navigationView) {
+    private void setupDrawerContent(final NavigationView navigationView) {
         navigationView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
                     @Override
-                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                         switch (menuItem.getItemId()) {
                             case R.id.list_navigation_menu_item:
-                                // Do nothing, we're already on that screen
+                                if (ab != null)
+                                    ab.setTitle(R.string.app_name);
+                                showTasksFragment(savedInstanceState);
+
+
                                 break;
                             case R.id.statistics_navigation_menu_item:
-                                Intent intent =
-                                        new Intent(TasksActivity.this, StatisticsActivity.class);
-                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-                                        | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                                startActivity(intent);
+                                if (ab != null)
+                                    ab.setTitle(R.string.statistics_title);
+                                showStatisticsFragment();
+
                                 break;
                             default:
                                 break;
                         }
                         // Close the navigation drawer when an item is selected.
-                        menuItem.setChecked(true);
                         mDrawerLayout.closeDrawers();
                         return true;
                     }
