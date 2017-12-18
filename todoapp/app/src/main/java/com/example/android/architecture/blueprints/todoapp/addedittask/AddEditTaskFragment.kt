6a5b4ad391264b9f226db1b5cd139a4b66e3a95c
Douglas Sigelbaum <sigelbaum@google.com>
2017-12-18 14:28:25 -0800

    Adding dokka plugin.

--- todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/addedittask/AddEditTaskFragment.kt
+++ todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/addedittask/AddEditTaskFragment.kt
@@ -56,6 +56,9 @@ class AddEditTaskFragment : Fragment(), AddEditTaskContract.View {
         }
     }
 
+    /**
+     * This is a test of Kdocs. Here is a link to [that method][showEmptyTaskError].
+     */
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?): View? {
         val root = inflater.inflate(R.layout.addtask_frag, container, false)
