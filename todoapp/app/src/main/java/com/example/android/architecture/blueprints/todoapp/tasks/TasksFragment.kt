025d69ebfbead2e10784ed457bd3e3d92fd82a6d
Doug Sigelbaum <sigelbaum@google.com>
2018-06-21 05:24:47 -0400

    Update dependency versions and fix nullability errors. (#557)
    * Update gradle versions and fix compile errors.
    
    * Add android P licenses for CI.
    
    * Fix licenses dir name typo.
    
    * Revert "Fix licenses dir name typo."
    
    This reverts commit b07f1a9d45911f02dc16a7db43eca0174137f23f.
    
    * Revert "Add android P licenses for CI."
    
    This reverts commit be164c7c0f343deae920505f73354629d09c3ff2.
    
    * Revert "Revert "Add android P licenses for CI.""
    
    This reverts commit 481c936f377a08c50522b53ca820f271b13ec5fa.
    
    * Revert "Revert "Fix licenses dir name typo.""
    
    This reverts commit 5dfd9e6dc468635cca5458b4d600e5eeee2aefe7.

--- todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksFragment.kt
+++ todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksFragment.kt
@@ -98,9 +98,9 @@ class TasksFragment : Fragment(), TasksContract.View {
             // Set up progress indicator
             findViewById<ScrollChildSwipeRefreshLayout>(R.id.refresh_layout).apply {
                 setColorSchemeColors(
-                        ContextCompat.getColor(activity, R.color.colorPrimary),
-                        ContextCompat.getColor(activity, R.color.colorAccent),
-                        ContextCompat.getColor(activity, R.color.colorPrimaryDark)
+                        ContextCompat.getColor(requireContext(), R.color.colorPrimary),
+                        ContextCompat.getColor(requireContext(), R.color.colorAccent),
+                        ContextCompat.getColor(requireContext(), R.color.colorPrimaryDark)
                 )
                 // Set the scrolling view in the custom SwipeRefreshLayout.
                 scrollUpChild = listView
@@ -120,7 +120,7 @@ class TasksFragment : Fragment(), TasksContract.View {
         }
 
         // Set up floating action button
-        activity.findViewById<FloatingActionButton>(R.id.fab_add_task).apply {
+        requireActivity().findViewById<FloatingActionButton>(R.id.fab_add_task).apply {
             setImageResource(R.drawable.ic_add)
             setOnClickListener { presenter.addNewTask() }
         }
@@ -143,6 +143,8 @@ class TasksFragment : Fragment(), TasksContract.View {
     }
 
     override fun showFilteringPopUpMenu() {
+        val activity = activity ?: return
+        val context = context ?: return
         PopupMenu(context, activity.findViewById(R.id.menu_filter)).apply {
             menuInflater.inflate(R.menu.filter_tasks, menu)
             setOnMenuItemClickListener { item ->
