518d445bea166bac546caa13eda7734d53ccd739
Jose Alcerreca <jalc@google.com>
2019-02-05 19:28:03 +0100

    Deletes temp files and fixes bad R import

--- todoapp/app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksScreenTest.kt
+++ todoapp/app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksScreenTest.kt
@@ -41,6 +41,8 @@ import androidx.test.filters.SdkSuppress
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
 import com.example.android.architecture.blueprints.todoapp.Injection
+import com.example.android.architecture.blueprints.todoapp.R.id
+import com.example.android.architecture.blueprints.todoapp.R.string
 import com.example.android.architecture.blueprints.todoapp.ViewModelFactory
 import com.example.android.architecture.blueprints.todoapp.currentActivity
 import com.example.android.architecture.blueprints.todoapp.getToolbarNavigationContentDescription
@@ -103,8 +105,7 @@ import org.junit.runner.RunWith
     }
 
     private val toolbarNavigationContentDescription: String
-        get() = tasksActivityTestRule.activity.getToolbarNavigationContentDescription(
-            com.example.android.architecture.blueprints.todoapp.R.id.toolbar)
+        get() = tasksActivityTestRule.activity.getToolbarNavigationContentDescription(id.toolbar)
 
     /**
      * A custom [Matcher] which matches an item in a [ListView] by its text.
@@ -125,17 +126,17 @@ import org.junit.runner.RunWith
             ).matches(item)
 
             override fun describeTo(description: Description) {
-                description.appendText("is isDescendantOfA LV with text " + itemText)
+                description.appendText("is isDescendantOfA LV with text $itemText")
             }
         }
     }
 
     @Test fun clickAddTaskButton_opensAddTaskUi() {
         // Click on the add task button
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.fab_add_task)).perform(click())
+        onView(withId(id.fab_add_task)).perform(click())
 
         // Check if the add task screen is displayed
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.add_task_title)).check(matches(isDisplayed()))
+        onView(withId(id.add_task_title)).check(matches(isDisplayed()))
     }
 
     @Test fun editTask() {
@@ -146,19 +147,19 @@ import org.junit.runner.RunWith
         onView(withText(TITLE1)).perform(click())
 
         // Click on the edit task button
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.fab_edit_task)).perform(click())
+        onView(withId(id.fab_edit_task)).perform(click())
 
         val editTaskTitle = TITLE2
         val editTaskDescription = "New Description"
 
         // Edit task title and description
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.add_task_title))
+        onView(withId(id.add_task_title))
                 .perform(replaceText(editTaskTitle), closeSoftKeyboard()) // Type new task title
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.add_task_description)).perform(replaceText(editTaskDescription),
+        onView(withId(id.add_task_description)).perform(replaceText(editTaskDescription),
                 closeSoftKeyboard()) // Type new task description and close the keyboard
 
         // Save the task
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.fab_edit_task_done)).perform(click())
+        onView(withId(id.fab_edit_task_done)).perform(click())
 
         // Verify task is displayed on screen in the task list.
         onView(withItemText(editTaskTitle)).check(matches(isDisplayed()))
@@ -257,7 +258,7 @@ import org.junit.runner.RunWith
 
         // Click clear completed in menu
         openActionBarOverflowOrOptionsMenu(getTargetContext())
-        onView(withText(com.example.android.architecture.blueprints.todoapp.R.string.menu_clear)).perform(click())
+        onView(withText(string.menu_clear)).perform(click())
 
         //Verify that completed tasks are not shown
         onView(withItemText(TITLE1)).check(matches(not(isDisplayed())))
@@ -274,7 +275,7 @@ import org.junit.runner.RunWith
         onView(withText(TITLE1)).perform(click())
 
         // Click delete task in menu
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.menu_delete)).perform(click())
+        onView(withId(id.menu_delete)).perform(click())
 
         // Verify it was deleted
         viewAllTasks()
@@ -290,7 +291,7 @@ import org.junit.runner.RunWith
         onView(withText(TITLE2)).perform(click())
 
         // Click delete task in menu
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.menu_delete)).perform(click())
+        onView(withId(id.menu_delete)).perform(click())
 
         // Verify only one task was deleted
         viewAllTasks()
@@ -308,13 +309,13 @@ import org.junit.runner.RunWith
         onView(withText(TITLE1)).perform(click())
 
         // Click on the checkbox in task details screen
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.task_detail_complete)).perform(click())
+        onView(withId(id.task_detail_complete)).perform(click())
 
         // Click on the navigation up button to go back to the list
         onView(withContentDescription(toolbarNavigationContentDescription)).perform(click())
 
         // Check that the task is marked as completed
-        onView(allOf(withId(com.example.android.architecture.blueprints.todoapp.R.id.complete),
+        onView(allOf(withId(id.complete),
                 hasSibling(withText(TITLE1)))).check(matches(isChecked()))
     }
 
@@ -329,13 +330,13 @@ import org.junit.runner.RunWith
         onView(withText(TITLE1)).perform(click())
 
         // Click on the checkbox in task details screen
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.task_detail_complete)).perform(click())
+        onView(withId(id.task_detail_complete)).perform(click())
 
         // Click on the navigation up button to go back to the list
         onView(withContentDescription(toolbarNavigationContentDescription)).perform(click())
 
         // Check that the task is marked as active
-        onView(allOf(withId(com.example.android.architecture.blueprints.todoapp.R.id.complete),
+        onView(allOf(withId(id.complete),
                 hasSibling(withText(TITLE1)))).check(matches(not(isChecked())))
     }
 
@@ -349,16 +350,16 @@ import org.junit.runner.RunWith
         onView(withText(TITLE1)).perform(click())
 
         // Click on the checkbox in task details screen
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.task_detail_complete)).perform(click())
+        onView(withId(id.task_detail_complete)).perform(click())
 
         // Click again to restore it to original state
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.task_detail_complete)).perform(click())
+        onView(withId(id.task_detail_complete)).perform(click())
 
         // Click on the navigation up button to go back to the list
         onView(withContentDescription(toolbarNavigationContentDescription)).perform(click())
 
         // Check that the task is marked as active
-        onView(allOf(withId(com.example.android.architecture.blueprints.todoapp.R.id.complete),
+        onView(allOf(withId(id.complete),
                 hasSibling(withText(TITLE1)))).check(matches(not(isChecked())))
     }
 
@@ -373,16 +374,16 @@ import org.junit.runner.RunWith
         onView(withText(TITLE1)).perform(click())
 
         // Click on the checkbox in task details screen
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.task_detail_complete)).perform(click())
+        onView(withId(id.task_detail_complete)).perform(click())
 
         // Click again to restore it to original state
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.task_detail_complete)).perform(click())
+        onView(withId(id.task_detail_complete)).perform(click())
 
         // Click on the navigation up button to go back to the list
         onView(withContentDescription(toolbarNavigationContentDescription)).perform(click())
 
         // Check that the task is marked as active
-        onView(allOf(withId(com.example.android.architecture.blueprints.todoapp.R.id.complete),
+        onView(allOf(withId(id.complete),
                 hasSibling(withText(TITLE1)))).check(matches(isChecked()))
     }
 
@@ -422,7 +423,7 @@ import org.junit.runner.RunWith
 
         // then nothing changes
         onView(withText(TITLE1)).check(matches(isDisplayed()))
-        onView(withText(com.example.android.architecture.blueprints.todoapp.R.string.label_completed)).check(matches(isDisplayed()))
+        onView(withText(string.label_completed)).check(matches(isDisplayed()))
     }
 
     // Blinking cursor after rotation breaks this in API 19
@@ -434,17 +435,17 @@ import org.junit.runner.RunWith
         onView(withText(TITLE1)).perform(click())
 
         // Click on the edit task button
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.fab_edit_task)).perform(click())
+        onView(withId(id.fab_edit_task)).perform(click())
 
         // Change task title (but don't save)
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.add_task_title))
+        onView(withId(id.add_task_title))
                 .perform(replaceText(TITLE2), closeSoftKeyboard()) // Type new task title
 
         // Rotate the screen
         currentActivity.rotateOrientation()
 
         // Verify task title is restored
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.add_task_title)).check(matches(withText(TITLE2)))
+        onView(withId(id.add_task_title)).check(matches(withText(TITLE2)))
     }
 
     // Blinking cursor after rotation breaks this in API 19
@@ -456,19 +457,19 @@ import org.junit.runner.RunWith
         onView(withText(TITLE1)).perform(click())
 
         // Click on the edit task button
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.fab_edit_task)).perform(click())
+        onView(withId(id.fab_edit_task)).perform(click())
 
         // Rotate the screen
         currentActivity.rotateOrientation()
 
         // Edit task title and description
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.add_task_title))
+        onView(withId(id.add_task_title))
                 .perform(replaceText(TITLE2), closeSoftKeyboard()) // Type new task title
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.add_task_description)).perform(replaceText(DESCRIPTION),
+        onView(withId(id.add_task_description)).perform(replaceText(DESCRIPTION),
                 closeSoftKeyboard()) // Type new task description and close the keyboard
 
         // Save the task
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.fab_edit_task_done)).perform(click())
+        onView(withId(id.fab_edit_task_done)).perform(click())
 
         // Verify task is displayed on screen in the task list.
         onView(withItemText(TITLE2)).check(matches(isDisplayed()))
@@ -482,7 +483,7 @@ import org.junit.runner.RunWith
         viewAllTasks()
 
         // Add task View should be displayed
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.noTasksAdd)).check(matches(isDisplayed()))
+        onView(withId(id.noTasksAdd)).check(matches(isDisplayed()))
     }
 
     @Test fun noTasks_CompletedTasksFilter_AddTaskViewNotVisible() {
@@ -490,7 +491,7 @@ import org.junit.runner.RunWith
         viewCompletedTasks()
 
         // Add task View should be displayed
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.noTasksAdd)).check(matches(not(isDisplayed())))
+        onView(withId(id.noTasksAdd)).check(matches(not(isDisplayed())))
     }
 
     @Test fun noTasks_ActiveTasksFilter_AddTaskViewNotVisible() {
@@ -498,40 +499,40 @@ import org.junit.runner.RunWith
         viewActiveTasks()
 
         // Add task View should be displayed
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.noTasksAdd)).check(matches(not(isDisplayed())))
+        onView(withId(id.noTasksAdd)).check(matches(not(isDisplayed())))
     }
 
     private fun viewAllTasks() {
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.menu_filter)).perform(click())
-        onView(withText(com.example.android.architecture.blueprints.todoapp.R.string.nav_all)).perform(click())
+        onView(withId(id.menu_filter)).perform(click())
+        onView(withText(string.nav_all)).perform(click())
     }
 
     private fun viewActiveTasks() {
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.menu_filter)).perform(click())
-        onView(withText(com.example.android.architecture.blueprints.todoapp.R.string.nav_active)).perform(click())
+        onView(withId(id.menu_filter)).perform(click())
+        onView(withText(string.nav_active)).perform(click())
     }
 
     private fun viewCompletedTasks() {
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.menu_filter)).perform(click())
-        onView(withText(com.example.android.architecture.blueprints.todoapp.R.string.nav_completed)).perform(click())
+        onView(withId(id.menu_filter)).perform(click())
+        onView(withText(string.nav_completed)).perform(click())
     }
 
     private fun createTask(title: String, description: String) {
         // Click on the add task button
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.fab_add_task)).perform(click())
+        onView(withId(id.fab_add_task)).perform(click())
 
         // Add task title and description
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.add_task_title)).perform(typeText(title),
+        onView(withId(id.add_task_title)).perform(typeText(title),
                 closeSoftKeyboard()) // Type new task title
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.add_task_description)).perform(typeText(description),
+        onView(withId(id.add_task_description)).perform(typeText(description),
                 closeSoftKeyboard()) // Type new task description and close the keyboard
 
         // Save the task
-        onView(withId(com.example.android.architecture.blueprints.todoapp.R.id.fab_edit_task_done)).perform(click())
+        onView(withId(id.fab_edit_task_done)).perform(click())
     }
 
     private fun clickCheckBoxForTask(title: String) {
-        onView(allOf(withId(com.example.android.architecture.blueprints.todoapp.R.id.complete), hasSibling(withText(title)))).perform(click())
+        onView(allOf(withId(id.complete), hasSibling(withText(title)))).perform(click())
     }
 
     private fun getText(stringId: Int) =
