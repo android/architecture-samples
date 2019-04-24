1a99b87ecefe9538c9891ea5670517d6286d038d
Jonathan Gerrish <jongerrish@google.com>
2019-04-24 12:27:03 -0700

    Cleanup TasksFragmentTest
    1) Remove @Ignore since this bug is now fixed
    2) Remove custom ViewMatcher - we don't want to promote these
    3) Fix failing tests by using doesNotExist() for items that are not
    present.
    4) Inline all the String constants, makes text entry read more clearly.
    5) Inline helper methods to make tests read more clear
    
    Change-Id: I0ee742b2f7620adfde510243622c35a689a4a636

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksFragmentTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksFragmentTest.kt
@@ -2,38 +2,29 @@ package com.example.android.architecture.blueprints.todoapp.tasks
 
 import android.content.Context
 import android.os.Bundle
-import android.view.View
-import android.widget.ListView
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
-import androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
-import androidx.test.filters.SmallTest
 import com.example.android.architecture.blueprints.todoapp.R
 import com.example.android.architecture.blueprints.todoapp.ServiceLocator
 import com.example.android.architecture.blueprints.todoapp.data.Task
 import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
+import com.example.android.architecture.blueprints.todoapp.util.deleteAllTasksBlocking
 import com.example.android.architecture.blueprints.todoapp.util.saveTaskBlocking
-import org.hamcrest.CoreMatchers.allOf
-import org.hamcrest.Description
-import org.hamcrest.Matcher
-import org.hamcrest.TypeSafeMatcher
 import org.hamcrest.core.IsNot.not
+import org.junit.After
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito.mock
@@ -44,6 +35,7 @@ import org.robolectric.annotation.TextLayoutMode
 /**
  * Integration test for the Task List screen.
  */
+// TODO - Use FragmentScenario, see: https://github.com/android/android-test/issues/291
 @RunWith(AndroidJUnit4::class)
 @MediumTest
 @LooperMode(LooperMode.Mode.PAUSED)
@@ -57,27 +49,9 @@ class TasksFragmentTest {
         repository = ServiceLocator.provideTasksRepository(getApplicationContext())
     }
 
-    /**
-     * A custom [Matcher] which matches an item in a [ListView] by its text.
-     *
-     *
-     * View constraint: View must be a child of a [ListView]
-     *
-     * @param itemText the text to match
-     *
-     * @return Matcher that matches text in the given view
-     */
-    private fun withItemText(itemText: String): Matcher<View> {
-        return object : TypeSafeMatcher<View>() {
-            override fun matchesSafely(item: View) = allOf(
-              isDescendantOfA(isAssignableFrom(ListView::class.java)),
-              withText(itemText)
-            ).matches(item)
-
-            override fun describeTo(description: Description) {
-                description.appendText("is isDescendantOfA LV with text $itemText")
-            }
-        }
+    @After
+    fun tearDown() {
+        repository.deleteAllTasksBlocking()
     }
 
     @Test
@@ -109,101 +83,79 @@ class TasksFragmentTest {
         launchFragmentInContainer<TasksFragment>(Bundle(), R.style.AppTheme)
 
         // THEN - Verify task is displayed on screen
-        onView(withItemText("title")).check(matches(isDisplayed()))
+        onView(withText("title")).check(matches(isDisplayed()))
     }
 
     @Test
-    @Ignore("blocked on robolectric/4862")
     fun displayActiveTask() {
-        repository.saveTaskBlocking(Task(TITLE1, DESCRIPTION1))
+        repository.saveTaskBlocking(Task("TITLE1", "DESCRIPTION1"))
 
-        val activityScenario = ActivityScenario.launch(TasksActivity::class.java)
+        ActivityScenario.launch(TasksActivity::class.java)
 
-        onView(withText(TITLE1)).check(matches(isDisplayed()))
+        onView(withText("TITLE1")).check(matches(isDisplayed()))
 
-        viewActiveTasks()
-        onView(withText(TITLE1)).check(matches(isDisplayed()))
+        onView(withId(R.id.menu_filter)).perform(click())
+        onView(withText(R.string.nav_active)).perform(click())
+        onView(withText("TITLE1")).check(matches(isDisplayed()))
 
-        viewCompletedTasks()
-        onView(withText(TITLE1)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.menu_filter)).perform(click())
+        onView(withText(R.string.nav_completed)).perform(click())
+        onView(withText("TITLE1")).check(matches(not(isDisplayed())))
     }
 
     @Test
-    @Ignore("blocked on robolectric/4862")
     fun displayCompletedTask() {
-        repository.saveTaskBlocking(Task(TITLE1, DESCRIPTION1, true))
+        repository.saveTaskBlocking(Task("TITLE1", "DESCRIPTION1", true))
 
-        val activityScenario = ActivityScenario.launch(TasksActivity::class.java)
+        ActivityScenario.launch(TasksActivity::class.java)
 
-        onView(withText(TITLE1)).check(matches(isDisplayed()))
+        onView(withText("TITLE1")).check(matches(isDisplayed()))
 
-        viewActiveTasks()
-        onView(withText(TITLE1)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.menu_filter)).perform(click())
+        onView(withText(R.string.nav_active)).perform(click())
+        onView(withText("TITLE1")).check(matches(not(isDisplayed())))
 
-        viewCompletedTasks()
-        onView(withText(TITLE1)).check(matches(isDisplayed()))
+        onView(withId(R.id.menu_filter)).perform(click())
+        onView(withText(R.string.nav_completed)).perform(click())
+        onView(withText("TITLE1")).check(matches(isDisplayed()))
     }
 
     @Test
-    @Ignore("blocked on robolectric/4862")
-    fun deleteOneTest() {
-        repository.saveTaskBlocking(Task(TITLE1, DESCRIPTION1))
+    fun deleteOneTask() {
+        repository.saveTaskBlocking(Task("TITLE1", "DESCRIPTION1"))
 
-        val activityScenario = ActivityScenario.launch(TasksActivity::class.java)
+        ActivityScenario.launch(TasksActivity::class.java)
 
         // Open it in details view
-        onView(withText(TITLE1)).perform(ViewActions.click())
+        onView(withText("TITLE1")).perform(click())
 
         // Click delete task in menu
-        onView(withId(R.id.menu_delete)).perform(ViewActions.click())
+        onView(withId(R.id.menu_delete)).perform(click())
 
         // Verify it was deleted
-        viewAllTasks()
-        onView(withText(TITLE1)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.menu_filter)).perform(click())
+        onView(withText(R.string.nav_all)).perform(click())
+        onView(withText("TITLE1")).check(doesNotExist())
     }
 
     @Test
-    @Ignore("blocked on robolectric/4862")
-    fun deleteOneOfTwoTests() {
-        repository.saveTaskBlocking(Task(TITLE1, DESCRIPTION1))
-        repository.saveTaskBlocking(Task(TITLE2, DESCRIPTION2))
+    fun deleteOneOfTwoTasks() {
+        repository.saveTaskBlocking(Task("TITLE1", "DESCRIPTION1"))
+        repository.saveTaskBlocking(Task("TITLE2", "DESCRIPTION2"))
 
-        val activityScenario = ActivityScenario.launch(TasksActivity::class.java)
+        ActivityScenario.launch(TasksActivity::class.java)
 
         // Open it in details view
-        onView(withText(TITLE1)).perform(ViewActions.click())
+        onView(withText("TITLE1")).perform(click())
 
         // Click delete task in menu
-        onView(withId(R.id.menu_delete)).perform(ViewActions.click())
+        onView(withId(R.id.menu_delete)).perform(click())
 
         // Verify it was deleted
-        viewAllTasks()
-        onView(withText(TITLE1)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.menu_filter)).perform(click())
+        onView(withText(R.string.nav_all)).perform(click())
+        onView(withText("TITLE1")).check(doesNotExist())
         // but not the other one
-        onView(withText(TITLE2)).check(matches(isDisplayed()))
-    }
-
-    private fun viewAllTasks() {
-        onView(ViewMatchers.withId(R.id.menu_filter)).perform(ViewActions.click())
-        onView(withText(R.string.nav_all)).perform(ViewActions.click())
-    }
-
-    private fun viewActiveTasks() {
-        onView(ViewMatchers.withId(R.id.menu_filter)).perform(ViewActions.click())
-        onView(withText(R.string.nav_active)).perform(ViewActions.click())
-    }
-
-    private fun viewCompletedTasks() {
-        onView(ViewMatchers.withId(R.id.menu_filter)).perform(ViewActions.click())
-        onView(withText(R.string.nav_completed)).perform(ViewActions.click())
-    }
-
-    companion object {
-
-        const val TITLE1 = "TITLE1"
-        const val TITLE2 = "TITLE2"
-
-        const val DESCRIPTION1 = "DESCRIPTION1"
-        const val DESCRIPTION2 = "DESCRIPTION2"
+        onView(withText("TITLE2")).check(matches(isDisplayed()))
     }
 }
