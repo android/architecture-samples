9a82efe8bdee9b31ac96fdb1346267261461d635
Jim Clermonts <jimclermonts@gmail.com>
2019-10-16 11:20:48 +0200

    test commit.

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksActivity.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksActivity.kt
@@ -33,36 +33,36 @@ import com.google.android.material.navigation.NavigationView
  */
 class TasksActivity : AppCompatActivity() {
 
-    private lateinit var drawerLayout: DrawerLayout
-    private lateinit var appBarConfiguration: AppBarConfiguration
+	private lateinit var drawerLayout: DrawerLayout
+	private lateinit var appBarConfiguration: AppBarConfiguration
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.tasks_act)
-        setupNavigationDrawer()
-        setSupportActionBar(findViewById(R.id.toolbar))
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.tasks_act)
+		setupNavigationDrawer()
+		setSupportActionBar(findViewById(R.id.toolbar))
 
-        val navController: NavController = findNavController(R.id.nav_host_fragment)
-        appBarConfiguration =
-            AppBarConfiguration.Builder(R.id.tasks_fragment_dest, R.id.statistics_fragment_dest)
-                .setDrawerLayout(drawerLayout)
-                .build()
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        findViewById<NavigationView>(R.id.nav_view)
-            .setupWithNavController(navController)
-    }
+		val navController: NavController = findNavController(R.id.nav_host_fragment)
+		appBarConfiguration =
+				AppBarConfiguration.Builder(R.id.tasks_fragment_dest, R.id.statistics_fragment_dest)
+						.setDrawerLayout(drawerLayout)
+						.build()
+		setupActionBarWithNavController(navController, appBarConfiguration)
+		findViewById<NavigationView>(R.id.nav_view)
+				.setupWithNavController(navController)
+	}
 
-    override fun onSupportNavigateUp(): Boolean {
-        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration)
-            || super.onSupportNavigateUp()
-    }
+	override fun onSupportNavigateUp(): Boolean {
+		return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration)
+				|| super.onSupportNavigateUp()
+	}
 
-    private fun setupNavigationDrawer() {
-        drawerLayout = (findViewById<DrawerLayout>(R.id.drawer_layout))
-            .apply {
-                setStatusBarBackground(R.color.colorPrimaryDark)
-            }
-    }
+	private fun setupNavigationDrawer() {
+		drawerLayout = (findViewById<DrawerLayout>(R.id.drawer_layout))
+				.apply {
+					setStatusBarBackground(R.color.colorPrimaryDark)
+				}
+	}
 }
 
 // Keys for navigation
