74080bc97b90ac1817ea2b7b6d41371b2ede5756
DESKTOP-5UF71SN\uberchilly <uberchilly@gmail.com>
2021-07-21 12:04:10 +0200

    HiltExt returns ActivityScenario so that tests can use moveToState similar to regular launchInContainer

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/HiltExt.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/HiltExt.kt
@@ -38,7 +38,7 @@ inline fun <reified T : Fragment> launchFragmentInHiltContainer(
     fragmentArgs: Bundle? = null,
     @StyleRes themeResId: Int = R.style.FragmentScenarioEmptyFragmentActivityTheme,
     crossinline action: Fragment.() -> Unit = {}
-) {
+): ActivityScenario<HiltTestActivity> {
     val startActivityIntent = Intent.makeMainActivity(
         ComponentName(
             ApplicationProvider.getApplicationContext(),
@@ -49,7 +49,7 @@ inline fun <reified T : Fragment> launchFragmentInHiltContainer(
         themeResId
     )
 
-    ActivityScenario.launch<HiltTestActivity>(startActivityIntent).onActivity { activity ->
+    return ActivityScenario.launch<HiltTestActivity>(startActivityIntent).onActivity { activity ->
         val fragment: Fragment = activity.supportFragmentManager.fragmentFactory.instantiate(
             Preconditions.checkNotNull(T::class.java.classLoader),
             T::class.java.name
