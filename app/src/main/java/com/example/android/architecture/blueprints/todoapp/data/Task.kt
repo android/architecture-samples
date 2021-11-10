2bb329e4f05ade24311f7e74b9e9bdd60991c02c
kimjin <kimjin@nemustech.com>
2021-11-10 16:03:30 +0900

    add comment 1

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/data/Task.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/data/Task.kt
@@ -36,7 +36,7 @@ data class Task @JvmOverloads constructor(
     @ColumnInfo(name = "completed") var isCompleted: Boolean = false,
     @PrimaryKey @ColumnInfo(name = "entryid") var id: String = UUID.randomUUID().toString()
 ) {
-
+    // add comment
     val titleForList: String
         get() = if (title.isNotEmpty()) title else description
 
