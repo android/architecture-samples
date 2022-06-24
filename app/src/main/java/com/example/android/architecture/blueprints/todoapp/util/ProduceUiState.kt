bf1d066248cf498f952cfa0ec29288d9eb814be9
Manuel Vivo <mvivo@google.com>
2022-06-24 12:10:46 +0200

    Add support for up to 12 flows

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/util/ProduceUiState.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/util/ProduceUiState.kt
@@ -111,3 +111,224 @@ fun <T1, T2, T3, T4, T5, R> ViewModel.produceUiState(
         started = started,
         initialValue = initialValue
     )
+
+fun <T1, T2, T3, T4, T5, T6, R> ViewModel.produceUiState(
+    flow: Flow<T1>,
+    flow2: Flow<T2>,
+    flow3: Flow<T3>,
+    flow4: Flow<T4>,
+    flow5: Flow<T5>,
+    flow6: Flow<T6>,
+    initialValue: R,
+    started: SharingStarted = WhileUiSubscribed,
+    uiStateProducerBlock: (T1, T2, T3, T4, T5, T6) -> R
+): StateFlow<R> =
+    combine(
+        combine(flow, flow2, flow3, ::Triple),
+        combine(flow4, flow5, flow6, ::Triple)
+    ) { t1, t2 ->
+        uiStateProducerBlock(
+            t1.first, t1.second, t1.third,
+            t2.first, t2.second, t2.third
+        )
+    }
+        .stateIn(
+            scope = viewModelScope,
+            started = started,
+            initialValue = initialValue
+        )
+
+fun <T1, T2, T3, T4, T5, T6, T7, R> ViewModel.produceUiState(
+    flow: Flow<T1>,
+    flow2: Flow<T2>,
+    flow3: Flow<T3>,
+    flow4: Flow<T4>,
+    flow5: Flow<T5>,
+    flow6: Flow<T6>,
+    flow7: Flow<T7>,
+    initialValue: R,
+    started: SharingStarted = WhileUiSubscribed,
+    uiStateProducerBlock: (T1, T2, T3, T4, T5, T6, T7) -> R
+): StateFlow<R> =
+    combine(
+        combine(flow, flow2, flow3, ::Triple),
+        combine(flow4, flow5, flow6, ::Triple),
+        flow7
+    ) { t1, t2, t3 ->
+        uiStateProducerBlock(
+            t1.first, t1.second, t1.third,
+            t2.first, t2.second, t2.third,
+            t3
+        )
+    }
+        .stateIn(
+            scope = viewModelScope,
+            started = started,
+            initialValue = initialValue
+        )
+
+fun <T1, T2, T3, T4, T5, T6, T7, T8, R> ViewModel.produceUiState(
+    flow: Flow<T1>,
+    flow2: Flow<T2>,
+    flow3: Flow<T3>,
+    flow4: Flow<T4>,
+    flow5: Flow<T5>,
+    flow6: Flow<T6>,
+    flow7: Flow<T7>,
+    flow8: Flow<T8>,
+    initialValue: R,
+    started: SharingStarted = WhileUiSubscribed,
+    uiStateProducerBlock: (T1, T2, T3, T4, T5, T6, T7, T8) -> R
+): StateFlow<R> =
+    combine(
+        combine(flow, flow2, flow3, ::Triple),
+        combine(flow4, flow5, flow6, ::Triple),
+        combine(flow7, flow8, ::Pair)
+    ) { t1, t2, t3 ->
+        uiStateProducerBlock(
+            t1.first, t1.second, t1.third,
+            t2.first, t2.second, t2.third,
+            t3.first, t3.second
+        )
+    }
+        .stateIn(
+            scope = viewModelScope,
+            started = started,
+            initialValue = initialValue
+        )
+
+fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, R> ViewModel.produceUiState(
+    flow: Flow<T1>,
+    flow2: Flow<T2>,
+    flow3: Flow<T3>,
+    flow4: Flow<T4>,
+    flow5: Flow<T5>,
+    flow6: Flow<T6>,
+    flow7: Flow<T7>,
+    flow8: Flow<T8>,
+    flow9: Flow<T9>,
+    initialValue: R,
+    started: SharingStarted = WhileUiSubscribed,
+    uiStateProducerBlock: (T1, T2, T3, T4, T5, T6, T7, T8, T9) -> R
+): StateFlow<R> =
+    combine(
+        combine(flow, flow2, flow3, ::Triple),
+        combine(flow4, flow5, flow6, ::Triple),
+        combine(flow7, flow8, flow9, ::Triple)
+    ) { t1, t2, t3 ->
+        uiStateProducerBlock(
+            t1.first, t1.second, t1.third,
+            t2.first, t2.second, t2.third,
+            t3.first, t3.second, t3.third
+        )
+    }
+        .stateIn(
+            scope = viewModelScope,
+            started = started,
+            initialValue = initialValue
+        )
+
+fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, R> ViewModel.produceUiState(
+    flow: Flow<T1>,
+    flow2: Flow<T2>,
+    flow3: Flow<T3>,
+    flow4: Flow<T4>,
+    flow5: Flow<T5>,
+    flow6: Flow<T6>,
+    flow7: Flow<T7>,
+    flow8: Flow<T8>,
+    flow9: Flow<T9>,
+    flow10: Flow<T10>,
+    initialValue: R,
+    started: SharingStarted = WhileUiSubscribed,
+    uiStateProducerBlock: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10) -> R
+): StateFlow<R> =
+    combine(
+        combine(flow, flow2, flow3, ::Triple),
+        combine(flow4, flow5, flow6, ::Triple),
+        combine(flow7, flow8, flow9, ::Triple),
+        flow10
+    ) { t1, t2, t3, t4 ->
+        uiStateProducerBlock(
+            t1.first, t1.second, t1.third,
+            t2.first, t2.second, t2.third,
+            t3.first, t3.second, t3.third,
+            t4
+        )
+    }
+        .stateIn(
+            scope = viewModelScope,
+            started = started,
+            initialValue = initialValue
+        )
+
+fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, R> ViewModel.produceUiState(
+    flow: Flow<T1>,
+    flow2: Flow<T2>,
+    flow3: Flow<T3>,
+    flow4: Flow<T4>,
+    flow5: Flow<T5>,
+    flow6: Flow<T6>,
+    flow7: Flow<T7>,
+    flow8: Flow<T8>,
+    flow9: Flow<T9>,
+    flow10: Flow<T10>,
+    flow11: Flow<T11>,
+    initialValue: R,
+    started: SharingStarted = WhileUiSubscribed,
+    uiStateProducerBlock: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11) -> R
+): StateFlow<R> =
+    combine(
+        combine(flow, flow2, flow3, ::Triple),
+        combine(flow4, flow5, flow6, ::Triple),
+        combine(flow7, flow8, flow9, ::Triple),
+        combine(flow10, flow11, ::Pair),
+    ) { t1, t2, t3, t4 ->
+        uiStateProducerBlock(
+            t1.first, t1.second, t1.third,
+            t2.first, t2.second, t2.third,
+            t3.first, t3.second, t3.third,
+            t4.first, t4.second
+        )
+    }
+        .stateIn(
+            scope = viewModelScope,
+            started = started,
+            initialValue = initialValue
+        )
+
+fun <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, R> ViewModel.produceUiState(
+    flow: Flow<T1>,
+    flow2: Flow<T2>,
+    flow3: Flow<T3>,
+    flow4: Flow<T4>,
+    flow5: Flow<T5>,
+    flow6: Flow<T6>,
+    flow7: Flow<T7>,
+    flow8: Flow<T8>,
+    flow9: Flow<T9>,
+    flow10: Flow<T10>,
+    flow11: Flow<T11>,
+    flow12: Flow<T12>,
+    initialValue: R,
+    started: SharingStarted = WhileUiSubscribed,
+    uiStateProducerBlock: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12) -> R
+): StateFlow<R> =
+    combine(
+        combine(flow, flow2, flow3, ::Triple),
+        combine(flow4, flow5, flow6, ::Triple),
+        combine(flow7, flow8, flow9, ::Triple),
+        combine(flow10, flow11, flow12, ::Triple),
+    ) { t1, t2, t3, t4 ->
+        uiStateProducerBlock(
+            t1.first, t1.second, t1.third,
+            t2.first, t2.second, t2.third,
+            t3.first, t3.second, t3.third,
+            t4.first, t4.second, t4.third
+        )
+    }
+        .stateIn(
+            scope = viewModelScope,
+            started = started,
+            initialValue = initialValue
+        )
