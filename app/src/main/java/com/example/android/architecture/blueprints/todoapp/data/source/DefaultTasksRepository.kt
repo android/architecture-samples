f9cae2c94aec0558c683eb64c6fdb06a64844bd5
Don Turner <donturner@google.com>
2023-03-07 09:27:08 +0000

    Fix spotless

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/data/source/DefaultTasksRepository.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/data/source/DefaultTasksRepository.kt
@@ -17,11 +17,11 @@
 package com.example.android.architecture.blueprints.todoapp.data.source
 
 import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.source.local.TasksDao
 import com.example.android.architecture.blueprints.todoapp.data.toExternalModel
 import com.example.android.architecture.blueprints.todoapp.data.toLocalModel
 import com.example.android.architecture.blueprints.todoapp.data.toNetworkModel
 import com.example.android.architecture.blueprints.todoapp.data.toTaskEntity
-import com.example.android.architecture.blueprints.todoapp.data.source.local.TasksDao
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
