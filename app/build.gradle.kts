36810e40f41246b19a3edf46629a50d96ed53bd2
MJJacobs01 <marthinusjacobs@gmail.com>
2023-08-12 11:20:13 +0200

    Updated annotation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,14 +81,14 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
-    packagingOptions {
+    packaging {
         resources {
             excludes += setOf("META-INF/AL2.0", "META-INF/LGPL2.1")
         }
--- shared-test/build.gradle.kts
+++ shared-test/build.gradle.kts
@@ -27,6 +27,19 @@ android {
     defaultConfig {
         minSdk = libs.versions.minSdk.get().toInt()
     }
+
+    java{
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions{
+        jvmTarget = "17"
+    }
+
+    kotlin{
+        jvmToolchain(17)
+    }
 }
 
 dependencies {
