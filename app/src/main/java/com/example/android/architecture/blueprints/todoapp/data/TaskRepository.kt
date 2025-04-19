708972944018dc7bbef04458c747bd3c9729cf8f
David Williams <dwillia328@gmail.com>
2025-04-19 00:26:44 -0700

    Code review DefaultTaskReopsitory

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/data/TaskRepository.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/data/TaskRepository.kt
@@ -22,19 +22,47 @@ import kotlinx.coroutines.flow.Flow
  * Interface to the data layer.
  */
 interface TaskRepository {
-
+    /**
+     * Get Flow(stream) of List<Task> from local data source
+     */
     fun getTasksStream(): Flow<List<Task>>
 
+    /**
+     * get List<Task> for local data source
+     * @param forceUpdate: If true, call refresh() to clear local data source and replace
+     * with data from remote data source
+     */
     suspend fun getTasks(forceUpdate: Boolean = false): List<Task>
 
+    /**
+     * Delete everything in the local data source and replace it with everything from the network
+     * data source.
+     */
     suspend fun refresh()
 
+    /**
+     * Observes a single task
+     * @param taskId: task to observe
+     */
     fun getTaskStream(taskId: String): Flow<Task?>
 
+    /**
+     * Get a Task with the given ID. Will return null if the task cannot be found.
+     *
+     * @param taskId - The ID of the task
+     * @param forceUpdate - true if the task should be updated from the network data source first.
+     */
     suspend fun getTask(taskId: String, forceUpdate: Boolean = false): Task?
 
+    /**
+     * Simply calls refresh()
+     * @param taskId - currently doesn't have a use.
+     */
     suspend fun refreshTask(taskId: String)
 
+    /**
+     * Insert or update a task in the database. If a task already exists, replace it.
+     */
     suspend fun createTask(title: String, description: String): String
 
     suspend fun updateTask(taskId: String, title: String, description: String)
